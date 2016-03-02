da9e99f4ac9aa36f0aad9b02f843e1760cf6d43b
BFetler <bfetler.gmail.com>
2016-03-02 17:30:22 -0500

    add kfold to predict_activity_clean

--- predict_activity_clean.py
+++ predict_activity_clean.py
@@ -4,7 +4,9 @@
 # use KFold cross validation
 
 import pandas as pd
+from functools import reduce
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import KFold
 
 from read_clean_data import readRawColumns, readRawData, \
     removeDuplicateColumns, rfFitScore, getImportantColumns
@@ -16,7 +18,36 @@ dftrain, dftrain_y, dftest, dftest_y = readRawData(dfcol)
 dftrain, dftest = removeDuplicateColumns(dfcol, dups, dftrain, dftest)
 
 clf = RandomForestClassifier(n_estimators=10)
-score, imp = rfFitScore(clf, dftrain, dftrain_y, dftest, dftest_y)
-impcol = getImportantColumns(dfcol, imp, 0.01)
+
+print("dftrain shape", dftrain.shape)
+
+def summit(impsum, imp):
+    if len(impsum) == 0:
+        impsum = imp
+    else:
+        impsum = [ x+y for x,y in zip(impsum, imp) ]
+    return impsum
+
+total_score = 0
+imps = []
+kf = KFold(dftrain.shape[0], n_folds=4)
+for train, test in kf:
+    subtrain, subtest, subtrain_y, subtest_y = dftrain.iloc[train], \
+      dftrain.iloc[test], dftrain_y.iloc[train], dftrain_y.iloc[test]
+    score, imp = rfFitScore(clf, subtrain, subtrain_y,subtest, subtest_y)
+    total_score += score
+#    imps.append(imp)
+    imps = summit(imps, imp)
+
+total_score *= 0.25
+print("total score", total_score)
+
+#imps = list(zip(*imps))
+#total_imp = [ sum(imp) for imp in imps ]
+print("imps len", len(imps), imps[:5])
+
+#clf = RandomForestClassifier(n_estimators=10)
+#score, imp = rfFitScore(clf, dftrain, dftrain_y, dftest, dftest_y)
+#impcol = getImportantColumns(dfcol, imp, 0.01)
 
 print("yoo hoo")
